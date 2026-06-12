05160137c762f330f026a0696ae3313b4d7a4d44
jeanluisgv <jguerrero.v03@gmail.com>
2026-06-12 18:22:38 -0500

    updated capabilities, manages scenes now

--- GameState.cpp
+++ GameState.cpp
@@ -1,8 +1,42 @@
 #include "GameState.h"
+#include "SceneLib.h"
 
-GameState::GameState()
+GameState::GameState() : mascot(), scene(idle0, action0, menuSprite), idle(true){}
+
+uint8_t GameState::getOption(){return scene.currOption;}
+uint8_t GameState::getScene(){return scene.currScene;}
+
+//this will be only done once we have secured that the user can change and has changed the option therefore we dont need 
+// to be given anything
+bool GameState::updateScene()
 {
-    mascot = Mascot();
-    scene = Scene();
-    idle = true;
+  switch(scene.currOption)
+  {
+    case 0:
+      if(scene.currOption == scene.currScene){return false;}
+      scene.idleBitmap = idle0;
+      scene.actionBitmap = action0;
+      break;
+    default:
+      return false;
+  }
+
+  scene.currScene = scene.currOption;
+  return true;
+}
+
+const uint16_t* GameState::getMenu(){return scene.menu[scene.currOption];}
+
+const uint16_t* GameState::getIdle(uint8_t i){return scene.idleBitmap[i];}
+
+void GameState::nextOption()
+{
+  if(scene.currOption >= 5){scene.currOption = 0;}
+  scene.currOption++;
+}
+
+void GameState::prevOption()
+{
+  if(scene.currOption <= 0){scene.currOption = 5;}
+  scene.currOption--;
 }
--- GameState.h
+++ GameState.h
@@ -3,6 +3,7 @@
 
 #include "Scene.h"
 #include "Mascot.h"
+#include "Scene.h"
 
 struct GameState
 {
@@ -11,7 +12,18 @@ struct GameState
   Scene scene;
   Mascot mascot;
 
-  bool idle;
+  bool idle; //is it in idle or action
+
+  uint8_t getScene();
+  uint8_t getOption();
+
+  const uint16_t* getMenu();
+  const uint16_t* getIdle(uint8_t i);
+
+  bool updateScene();
+
+  void nextOption();
+  void prevOption();
 };
 
 #endif
